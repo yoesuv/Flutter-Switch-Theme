95975f4a8050fbc30997d0a4c0fdec64bab18142
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-13 16:27:58 +0700

    :newspaper: create screen

---
+++ lib/src/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_switch_theme/src/ui/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Switch Theme',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
