cb258c4117876451662281a62867390dda4f1346
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-14 14:49:29 +0700

    :newspaper: my app bloc

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_switch_theme/src/my_app_bloc.dart';
+import 'package:flutter_switch_theme/src/my_app_event.dart';
+import 'package:flutter_switch_theme/src/my_app_state.dart';
 import 'package:flutter_switch_theme/src/route/app_route.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,24 +10,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Switch Theme',
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.teal,
-          brightness: Brightness.dark,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => MyAppBloc()..add(MyAppInitEvent())),
+      ],
+      child: BlocBuilder<MyAppBloc, MyAppState>(
+        buildWhen: (prev, current) => prev.themeData != current.themeData,
+        builder: (context, state) => MaterialApp(
+          title: 'Flutter Switch Theme',
+          theme: state.themeData.copyWith(
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.teal,
+            ),
+          ),
+          onGenerateRoute: AppRoute.routes,
         ),
-        useMaterial3: true,
       ),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.teal,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
-      onGenerateRoute: AppRoute.routes,
-      themeMode: ThemeMode.dark,
     );
   }
 }
---
+++ lib/src/my_app_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_switch_theme/src/my_app_event.dart';
+import 'package:flutter_switch_theme/src/my_app_state.dart';
+
+class MyAppBloc extends Bloc<MyAppEvent, MyAppState> {
+  MyAppBloc()
+      : super(MyAppState(themeData: ThemeData.light(useMaterial3: true))) {
+    on<MyAppInitEvent>(_onInit);
+    on<MyAppThemeChangedEvent>(_onChange);
+  }
+
+  void _onInit(MyAppInitEvent event, Emitter<MyAppState> emit) {}
+
+  void _onChange(MyAppThemeChangedEvent event, Emitter<MyAppState> emit) {}
+}
